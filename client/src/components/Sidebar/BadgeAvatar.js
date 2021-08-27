a7848d7a2b6ffc79542483c6044a7970512f1a77
Shree Devi Yadam <shree.yadam@gmail.com>
2021-08-27 13:27:42 -0400

    Bug fix: Sending messages #1 : New message immediately added to the chat UI

--- client/src/components/Sidebar/BadgeAvatar.js
+++ client/src/components/Sidebar/BadgeAvatar.js
@@ -33,7 +33,7 @@ const UserAvatar = (props) => {
         classes={{ badge: `${classes.badge} ${online && classes.online}` }}
         variant="dot"
         anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-        overlap="circlular">
+        overlap="circular">
         <Avatar alt={username} src={photoUrl} className={classes.profilePic}></Avatar>
       </Badge>
     </Box>
