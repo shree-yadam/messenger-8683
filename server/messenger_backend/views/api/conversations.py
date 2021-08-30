9617e0945df81d356e803eb870274c8f3e249f6a
Shree Devi Yadam <shree.yadam@gmail.com>
2021-08-30 18:45:54 -0400

    Display unread count in sidebar

--- server/messenger_backend/views/api/conversations.py
+++ server/messenger_backend/views/api/conversations.py
@@ -37,7 +37,7 @@ class Conversations(APIView):
                 convo_dict = {
                     "id": convo.id,
                     "messages": [
-                        message.to_dict(["id", "text", "senderId", "createdAt"])
+                        message.to_dict(["id", "text", "senderId", "createdAt", "read"])
                         for message in convo.messages.all()
                     ],
                 }
