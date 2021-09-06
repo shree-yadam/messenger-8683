cac40432573ef6ee05f53ae7f561506f920d4f27
Shree Devi Yadam <shree.yadam@gmail.com>
2021-09-05 23:23:42 -0400

    Add receiver image under last read message

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -6,13 +6,20 @@ import moment from "moment";
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
 
+  let lastReadIndex = undefined;
+  for (let i = 1; i < messages.length; i++){
+    if(!messages[i].read && messages[i-1].read){
+      lastReadIndex = i - 1;
+      break;
+    }
+  }
   return (
     <Box>
-      {messages.map((message) => {
+      {messages.map((message, index) => {
         const time = moment(message.createdAt).format("h:mm");
 
         return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
+          <SenderBubble key={message.id} text={message.text} time={time} isLastRead={index === lastReadIndex} otherUser={otherUser}/>
         ) : (
           <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
         );
